9f18d0543be14a4ae4dd03baca196a7382c66b56
AlbertoAlfredo <raioalfredo@gmail.com>
2022-06-22 00:28:53 -0300

    tentando configurar o deploy automatico

--- deploy.sh
+++ deploy.sh
@@ -6,21 +6,8 @@ set -e
 # build
 npm run build
 
-# navigate into the build output directory
-cd dist
+git add dist -f
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+git commit -m "deploy"
 
-git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:menurapido/menurapido.github.io main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-
-cd -
+git subtree push --prefix dist origin gh-pages
